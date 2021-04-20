7dd4b74fe749494e0d87ae126a01d934066195be
Daniele D'Agostino <principelele@gmail.com>
2021-04-20 16:57:30 +0200

    eureka server conf added

--- src/main/java/zuulserver/ZuulServerApplication.java
+++ src/main/java/zuulserver/ZuulServerApplication.java
@@ -2,6 +2,7 @@ package zuulserver;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 @EnableAutoConfiguration
 @Controller
 @EnableZuulProxy
+@EnableEurekaClient
 public class ZuulServerApplication {
   public static void main(String[] args) {
     new SpringApplicationBuilder(ZuulServerApplication.class).web(true).run(args);
